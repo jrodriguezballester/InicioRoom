0d21b99c4d5845fcab35b9a8b46b10a53a69c015
joser <jrodriguezballester@gmail.com>
2023-01-11 16:28:31 +0100

    mejora prescripcion

--- app/src/main/java/com/example/farma4/ui/prescripcion/PrescripcionViewAdapter.kt
+++ app/src/main/java/com/example/farma4/ui/prescripcion/PrescripcionViewAdapter.kt
@@ -11,18 +11,20 @@ import com.example.farma4.databinding.ItemPrescripcionBinding
 import com.example.farma4.tests.Utilidades
 import java.util.*
 
-class PrescripcionViewAdapter: RecyclerView.Adapter<PrescripcionViewHolder>() {
+class PrescripcionViewAdapter : RecyclerView.Adapter<PrescripcionViewHolder>() {
 
     private val medicinasList = ArrayList<Medicina>()
 
-    fun setList(medicinaList:List<Medicina>){
+    fun setList(medicinaList: List<Medicina>) {
         medicinasList.clear()
-        medicinasList.addAll( medicinaList)
+        medicinasList.addAll(medicinaList)
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PrescripcionViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding:ItemPrescripcionBinding=DataBindingUtil.inflate(layoutInflater, R.layout.item_prescripcion,parent,false)
-        return PrescripcionViewHolder(binding,parent.context)
+        val binding: ItemPrescripcionBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.item_prescripcion, parent, false)
+        return PrescripcionViewHolder(binding, parent.context)
 
     }
 
@@ -35,21 +37,19 @@ class PrescripcionViewAdapter: RecyclerView.Adapter<PrescripcionViewHolder>() {
     }
 
 }
-class PrescripcionViewHolder (val binding:ItemPrescripcionBinding,val context: Context) :RecyclerView.ViewHolder(binding.root){
+
+class PrescripcionViewHolder(val binding: ItemPrescripcionBinding, val context: Context) :
+    RecyclerView.ViewHolder(binding.root) {
     fun bind(medicina: Medicina) {
-        val numberOFDays: Long =
-            (medicina.FecFinTto.getTime() - Date().getTime()) / (3600 * 24 * 1000)
+
+        val diasToEndTto: Long = (medicina.FecFinTto.getTime() - Date().getTime()) / (3600000 * 24)
         binding.nameTextView.text = medicina.name
         binding.principioTextView.text = medicina.principio
-        binding.unidadesCajaTextView.text = medicina.unidadesCaja.toString()
-      //  binding.dosisTextView.text = medicina.dosis
-     //   binding.stockTextView.text = medicina.stock.toString()
-        binding.diasFinTtoTextView.text = numberOFDays.toString()
+
+        binding.diasFinTtoTextView.text = diasToEndTto.toString()
         binding.fecFinTtoTextView.text = Utilidades.dateToStringBarra(medicina.FecFinTto)
-//        binding.fecStockTextView.text = Utilidades.dateToStringBarra(medicina.fechaStock)
 
-
-        val cardColor: Int = Utilidades.calcularColorTto(context, numberOFDays)
+        val cardColor: Int = Utilidades.calcularColorTto(context, diasToEndTto)
         binding.cardView.setCardBackgroundColor(cardColor)
 
     }
